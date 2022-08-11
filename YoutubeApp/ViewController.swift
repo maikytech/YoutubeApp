56e5c172081584c172aec4f7da00fc55fee37f27
Maiqui Cedeño <maiqui.cedeno@gc.vasslatam.com>
2022-08-11 10:05:09 -0500

    Start project

---
+++ YoutubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by Maiqui Cedeño on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
