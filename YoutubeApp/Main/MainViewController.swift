cad350b178398dc46bdb02666fc3d06d28acc81b
Maiqui Cedeño <maiqui.cedeno@gc.vasslatam.com>
2022-09-07 20:16:32 -0500

    Set ContainerView in the mainStoryboard

---
+++ YoutubeApp/Main/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  YoutubeApp
+//
+//  Created by Maiqui Cedeño on 7/09/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
---
+++ YoutubeApp/Main/RootPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootPageViewController.swift
+//  YoutubeApp
+//
+//  Created by Maiqui Cedeño on 7/09/22.
+//
+
+import UIKit
+
+class RootPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
